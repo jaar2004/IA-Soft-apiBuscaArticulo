1420cac819ea0b53038b40fdc9b9963b9ef4b325
jaar2004 <iarsan_@hotmai.com>
2025-05-03 19:13:18 -0600

    upload initial projectproject to repository

---
+++ apiBuscaArticulo/apiBuscaArticulo/Class1.cs
@@ -0,0 +1,14 @@
+﻿// IAR jue 01MAY2025
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apiBuscaArticulo
+{
+    public class Class1
+    {
+    }
+}
